f10338eba0380a8b986a5703100fd7b20405820e
HENRION Mathilde <mathilde.henrion@etu.univ-littoral.fr>
2023-09-08 09:25:40 +0200

    création de la fenêtre

---
+++ Main.java
@@ -0,0 +1,7 @@
+import MG2D.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainGraphique graphique = new MainGraphique();
+    }
+}
---
+++ MainGraphique.java
@@ -0,0 +1,10 @@
+import MG2D.*;
+import MG2D.geometrie.*;
+
+public class MainGraphique {
+    
+    public MainGraphique() {
+        Fenetre f = new Fenetre("Jeu", 800, 700);
+        f.rafraichir();
+    }
+}
