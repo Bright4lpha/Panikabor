c48cd744e644fb03a541552dd1854bf00afee085
HENRION Mathilde <mathilde.henrion@etu.univ-littoral.fr>
2023-11-11 18:10:37 +0100

    Create Defaite.java

---
+++ Defaite.java
@@ -0,0 +1,33 @@
+import MG2D.*;
+import MG2D.geometrie.*;
+
+class Defaite {
+
+    public Defaite(Fenetre f) {
+        // f.ajouter(new Rectangle(Couleur.VERT, new Point(0, 0), 1000, 700, true));
+        f.ajouter(new Texture("./images/space.png", new Point(0, 0)));
+        f.ajouter(new Texture("./images/defaite.png", new Point(0, 0)));
+        f.ajouter(new Texture("./images/quitter.png", new Point(350, 0)));
+        // f.ajouter(new Texture("./images/retour.png", new Point(350, 0)));
+        f.rafraichir();
+    }
+
+    public void afficher(Fenetre f) {
+        // f.ajouter(new Rectangle(Couleur.VERT, new Point(0, 0), 1000, 700, true));
+        f.ajouter(new Texture("./images/space.png", new Point(0, 0)));
+        f.ajouter(new Texture("./images/defaite.png", new Point(0, 0)));
+        f.ajouter(new Texture("./images/quitter.png", new Point(350, 0)));
+        f.rafraichir();
+    }
+
+    public int deplacements_souris(Fenetre f, Point to) {
+        // bouton quitter
+        if ((to.getX()>=400) && (to.getX()<=600) && (to.getY()>=100) && (to.getY()<=200)) {
+            return 3;
+        }
+        // reste sur la page defaite
+        else {
+            return 6;
+        }
+    }
+}
